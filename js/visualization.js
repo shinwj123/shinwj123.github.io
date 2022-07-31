943e2e4d8a160734adada01cd3bb8beb862998ec
Shin, Jeff <wonjong2@illinois.edu>
2022-07-31 10:42:15 -0500

    update

--- js/visualization.js
+++ js/visualization.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+//============== env ===============//
+
+// Retrieve the scenes
+var scene1 = d3.select('#scene1')
+var scene2 = d3.select('#scene2')
+var scene3 = d3.select('#scene3')
+
+// constants
+var width = 900
+var height = 900
+
